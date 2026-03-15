8c8e9046cbce77bbeaeaf673b018eaf0c6db2bbc
IshikawaUta <jayaj7129@gmail.com>
2026-03-15 10:59:49 +0700

    update tester handling

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -10,7 +10,7 @@ describe Eksa::Application do
   it "returns 404 for unknown routes" do
     get '/unknown'
     expect(last_response.status).to eq(404)
-    expect(last_response.body).to include("Halaman tidak ditemukan")
+    expect(last_response.body).to include("Halaman Tidak Ditemukan")
   end
 
   it "can add and reach routes" do
